304e88081ce35071dc74741c3961fcf15f412231
Sujit Kumar Libi <sujit_libi@hotmail.com>
2024-11-08 11:13:25 +0545

    Modulo operator code added

--- Day 3/task/modulo_operator.py
+++ Day 3/task/modulo_operator.py
@@ -9,10 +9,18 @@
 # PAUSE 1 - What is 10 % 3?
 # What do you think this will output?
 #
-# print(10 % 3)
+print(10 % 3)
+# output: 1
 #
 # PAUSE 2 - Check Odd or Even
 # Write some code using what you have learnt about the modulo operator
 # and conditional checks in Python to check if the number in he input
 # area is odd or even.
 # If it's odd print out the word "Odd" otherwise printout "Even".
+
+print("Odd or Even Checker: ")
+number = int(input("Please enter any number: "))
+if number % 2 == 0:
+    print("Number you have entered is Even")
+else:
+    print("Number you have entered is Odd")
