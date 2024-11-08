d127f2a9dc92e763d6bd507af307861d36fbc7bd
Sujit Kumar Libi <sujit_libi@hotmail.com>
2024-11-08 11:07:20 +0545

    Day-3 Control flow and conditional operator code added

---
+++ Day 3/task/modulo_operator.py
@@ -0,0 +1,18 @@
+# The modulo operator gives you the remainder of a division.
+#
+# 6 % 2 #will be 0
+#
+# 6 % 5 #will be 1
+#
+# 6 % 4 #will be 2
+#
+# PAUSE 1 - What is 10 % 3?
+# What do you think this will output?
+#
+# print(10 % 3)
+#
+# PAUSE 2 - Check Odd or Even
+# Write some code using what you have learnt about the modulo operator
+# and conditional checks in Python to check if the number in he input
+# area is odd or even.
+# If it's odd print out the word "Odd" otherwise printout "Even".
