74e11f794e514837116aba5bedab1b767b6ead05
Sujit Kumar Libi <sujit_libi@hotmail.com>
2025-08-08 23:02:48 +0545

    Day 19 - Project - The Hirst

---
+++ Day 18 - Intermediate - Turtle and the Graphical User Interface/project/hirst_painting/main.py
@@ -0,0 +1,40 @@
+import random
+
+import colorgram
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+screen.colormode(255)
+
+rgb_colors = []
+colors = colorgram.extract('image.jpg', 30)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+
+tim.speed("fastest")
+tim.hideturtle()
+tim.penup()
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(rgb_colors))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+screen.exitonclick()
