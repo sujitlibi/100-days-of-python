f6e6d1e132e135a565c7d340e146c8ae83cb5686
Sujit Kumar Libi <sujit_libi@hotmail.com>
2025-08-10 14:57:41 +0545

    Day 20 and 21 - Snake Game: Controlling the Snake movement with Keypress

--- Day 20 - Intermediate - Building Snake Game /project/snake-game/main.py
+++ Day 20 - Intermediate - Building Snake Game /project/snake-game/main.py
@@ -16,6 +16,11 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+    screen.listen()
 
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
 
 screen.exitonclick()
--- Day 20 - Intermediate - Building Snake Game /project/snake-game/snake.py
+++ Day 20 - Intermediate - Building Snake Game /project/snake-game/snake.py
@@ -2,11 +2,17 @@ from turtle import Turtle
 
 STARTING_POSITIONS =   [(0, 0), (-20, 0), (-40, 0)]
 MOVING_DISTANCE = 20
+UP=90
+DOWN=270
+LEFT=180
+RIGHT=0
 
 class Snake:
     def __init__(self):
         self.body_segments = []
         self.create_snake()
+        self.snake_head = self.body_segments[0]
+
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -21,4 +27,20 @@ class Snake:
             new_x = self.body_segments[seg_num - 1].xcor()
             new_y = self.body_segments[seg_num - 1].ycor()
             self.body_segments[seg_num].goto(new_x, new_y)
-        self.body_segments[0].fd(MOVING_DISTANCE)
+        self.snake_head.fd(MOVING_DISTANCE)
+
+    def up(self):
+        if self.snake_head.heading() != DOWN:
+            self.snake_head.setheading(90)
+
+    def down(self):
+        if self.snake_head.heading() != UP:
+            self.snake_head.setheading(270)
+
+    def left(self):
+        if self.snake_head.heading() != RIGHT:
+            self.snake_head.setheading(180)
+
+    def right(self):
+        if self.snake_head.heading() !=LEFT:
+            self.snake_head.setheading(0)
