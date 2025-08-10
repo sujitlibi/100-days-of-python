188ba1c96a48b9af40e664a20fb80bf5d8bcbadd
Sujit Kumar Libi <sujit_libi@hotmail.com>
2025-08-10 13:05:23 +0545

    Day 20 and 21 - Snake Game: Create Snake body segment with initial position

--- Day 20 - Intermediate - Building Snake Game /project/snake-game/main.py
+++ Day 20 - Intermediate - Building Snake Game /project/snake-game/main.py
@@ -5,8 +5,11 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+starting_position =[(0,0), (-20,0), (-40,0)]
 
-
-
+for position in starting_position:
+    snake_body_segment = Turtle(shape="square")
+    snake_body_segment.color("white")
+    snake_body_segment.goto(position)
 
 screen.exitonclick()
