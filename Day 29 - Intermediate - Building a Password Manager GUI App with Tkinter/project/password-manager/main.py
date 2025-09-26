353402705bad1e322b4ddd730e665b89c6ce5c1e
Sujit Kumar Libi <sujit_libi@hotmail.com>
2025-09-26 20:23:24 +0545

    Day 29 - Intermediate - Building a Password Manager GUI App with Tkinter

--- Day 29 - Intermediate - Building a Password Manager GUI App with Tkinter/project/password-manager/main.py
+++ Day 29 - Intermediate - Building a Password Manager GUI App with Tkinter/project/password-manager/main.py
@@ -1,10 +1,47 @@
 from tkinter import *
+from tkinter import messagebox
+from random import randint, choice, shuffle
+import pyperclip
 
 NORMAL_FONT = ("Times New Roman", 18, "normal")
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+    # print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="OOPS", message="Please don't leave any field empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                              f"\nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("password.text", mode="a") as file:
+                file.write(f"\n{website} | {email} | {password}")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -43,11 +80,11 @@ password_label.grid(column=1, row=5)
 password_entry = Entry(width=21, fg="black", bg="white", highlightthickness=0)
 password_entry.grid(column=2, row=5)
 
-generate_password_btn = Button(text="Generate Password", fg="black", bg="white", highlightthickness=0, bd=0)
+generate_password_btn = Button(text="Generate Password", fg="black", bg="white", highlightthickness=0, bd=0, command=generate_password)
 generate_password_btn.grid(column=3, row=5)
 
 # Add Button Component
-add_info_btn = Button(text="Add", width=35, fg="black", bg="white", highlightthickness=0, bd=0)
+add_info_btn = Button(text="Add", width=35, fg="black", bg="white", highlightthickness=0, bd=0, command=save_password)
 add_info_btn.grid(column=2, row=6, columnspan=2)
 
 
