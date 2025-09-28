8aa0a2a50cfa8fc74c0c8a82480b0aa6874bb5b0
Sujit Kumar Libi <sujit_libi@hotmail.com>
2025-09-28 10:01:08 +0545

    Day 30 - Intermediate - Errors, Exceptions and JSON Data: Improving the Password Manager App - Password Search Feature From  JSON

--- Day 30 - Intermediate - Errors, Exceptions and JSON Data: Improving the Password Manager App/project/password-manager/main.py
+++ Day 30 - Intermediate - Errors, Exceptions and JSON Data: Improving the Password Manager App/project/password-manager/main.py
@@ -26,6 +26,36 @@ def generate_password():
     pyperclip.copy(password)
 
     # print(f"Your password is: {password}")
+
+# ---------------------------- SEARCH WEBSITE ------------------------------ #
+def search_info():
+    try:
+        with open("password.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Oops", message="Information no available")
+    else:
+        title = website_entry.get()
+        try:
+            fetch_data = data[title]
+            messagebox.askokcancel(title=title, message=f"email: {fetch_data["email"]} \n password: {fetch_data["password"]}")
+        except KeyError:
+            messagebox.showinfo(title="Oops", message="Information no available")
+
+    # Angela solution
+    # website = website_entry.get()
+    # try:
+    #     with open("data.json") as data_file:
+    #         data = json.load(data_file)
+    # except FileNotFoundError:
+    #     messagebox.showinfo(title="Error", message="No Data File Found.")
+    # else:
+    #     if website in data:
+    #         email = data[website]["email"]
+    #         password = data[website]["password"]
+    #         messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+    #     else:
+    #         messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
     website = website_entry.get()
@@ -45,17 +75,17 @@ def save_password():
             with open("password.json", mode="r") as file:
                 # Load Data
                 data = json.load(file)
-                # update old data with new data
-                data.update(new_entry)
         except FileNotFoundError:
             with open("password.json", mode="w") as file:
                 json.dump(new_entry, file, indent=4)
         else:
+            # update old data with new data
+            data.update(new_entry)
             with open("password.json", mode="w") as file:
                 json.dump(data, file, indent=4)
-
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -76,16 +106,20 @@ canvas.grid(column=2, row=2)
 website_label = Label(text="Website:", fg="black", bg="white", font=NORMAL_FONT)
 website_label.grid(column=1, row=3)
 
-website_entry = Entry(width=35, fg="black", bg="white", highlightthickness=0)
-website_entry.grid(column=2, row=3, columnspan=2)
+website_entry = Entry(width=21, fg="black", bg="white", highlightthickness=0)
+website_entry.grid(column=2, row=3)
 website_entry.focus()
+
+# Search Component
+search_btn = Button(text="Search", width=13, fg="black", bg="white", highlightthickness=0, command=search_info)
+search_btn.grid(column=3, row=3)
 
 # Email / Username Component
 email_label = Label(text="Email/Username:", fg="black", bg="white", font=NORMAL_FONT)
 email_label.grid(column=1, row=4)
 
 email_entry = Entry(width=35, fg="black", bg="white", highlightthickness=0)
-email_entry.grid(column=2, row=4, columnspan=2)
+email_entry.grid(column=2, row=4, columnspan=3)
 
 # Password Component
 password_label = Label(text="Password:", fg="black", bg="white", font=NORMAL_FONT)
@@ -99,7 +133,7 @@ generate_password_btn.grid(column=3, row=5)
 
 # Add Button Component
 add_info_btn = Button(text="Add", width=35, fg="black", bg="white", highlightthickness=0, bd=0, command=save_password)
-add_info_btn.grid(column=2, row=6, columnspan=2)
+add_info_btn.grid(column=2, row=6, columnspan=3)
 
 
 window.mainloop()
